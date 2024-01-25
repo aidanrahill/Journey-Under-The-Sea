b17dc68565a153ec306a08e778591c8f40322b90
danyul <danieltonyh06@gmail.com>
2024-01-25 10:23:08 -0800

    win and lose screen with functional buttons

--- Assets/Scripts/LobbySceneLoader.cs
+++ Assets/Scripts/LobbySceneLoader.cs
@@ -7,4 +7,17 @@ public class LobbySceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene("Lobby Scene"); // Make sure the scene name matches exactly
     }
+    public void LoadMenuScene ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void quit()
+{
+    #if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+    #else
+    Application.Quit();
+    #endif
+}
+
 }
